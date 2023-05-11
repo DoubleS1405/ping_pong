919d48c65e63ce88c99f33901b93214bb3132899
Jong Moon <jongmoon@hacksper.com>
2023-05-11 21:54:00 +0900

    ping, http check 함수 테스트

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
+import pythonping
+import requests
 
-def print_hi(name):
-  print(f'Hi, {name}')
+
+# hostname: ip or domain
+
+
+def check_ping(hostname):
+  """
+  핑 테스트 함수
+  :param hostname:
+  :return:
+  """
+  res = pythonping.ping(hostname, count=3, interval=1)  # verbose=True,
+
+  for item in res:
+    print(item)  # 개별 출력
+
+
+def check_http(url):
+  """
+  http 테스트 함수
+  :param url: 
+  :return: 
+  """
+  response = requests.get(url)
+  print(response.status_code)
+  # print(response.text)
 
 
 if __name__ == '__main__':
-  print_hi('PyCharm')
+  ####################
+  # ping
+  # check_ping('127.0.0.1')
+  # check_ping('blog.hacksper.com')
+
+  ####################
+  # http
+  check_http('https://blog.hacksper.com')
+
+
+
